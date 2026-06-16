164bc6f6f1e2f861bc083f2548e086a7cbbd47d7
murilo fernandes <mufernandes@yahoo.com.br>
2026-06-16 01:04:39 -0300

    chore: repository cleanup - removed legacy deploy scripts and documentation

--- scripts/step5a_nginx_read.py
+++
@@ -1,5 +0,0 @@
-"""Etapa 5 - Ler config nginx atual para planejar insercao dos headers."""
-from _ssh import conn, run
-c = conn()
-run(c, "cat -n /etc/nginx/sites-available/relm-careplus")
-c.close()
